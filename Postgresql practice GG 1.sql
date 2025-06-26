73e03d195530b10e35afb4ae78167b2226159c2c
Mahesh Thakare <maheshthakare225@gmail.com>
2025-06-26 17:55:43 +0530

    Update Postgresql practice GG 1.sql

--- Postgresql practice GG 1.sql
+++ Postgresql practice GG 1.sql
@@ -147,3 +147,4 @@ INNER JOIN Borrowings ON Members.member_id = Borrowings.member_id
 INNER JOIN Books ON Borrowings.book_id = Books.book_id 
 WHERE Books.title = '1984';
 
+--finish Beginer Level--
